f279875162e23b27aefbfe1d6f3a202ba95f35f7
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-07-31 12:09:07 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int range,limit;
+		Scanner sc=new Scanner(System.in);
+		limit=sc.nextInt();
+		range=sc.nextInt();
+		for(int i=limit+1; i<range-1;i++)
+		{
+		           if( i%2== 0)	
+		
+			{
+				System.out.print(i);
+			}
+		}	
+		
+	}
+}
